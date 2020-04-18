636df2be2be53ba06a99585f6ea06b531af5990e
jimmyyukio <1425717639@qq.com>
2020-04-18 14:49:13 +0800

    2020年4月18日14:49:09

--- c程序文件/scanf用法/Project1/源.c
+++
@@ -1,8 +0,0 @@
-# include <stdio.h>
-int main(void)
-{
-	int i;
-	scanf("%d", &i);
-	printf("i=%d\n", i);
-	return 0;
-}
--- c程序文件/非输出控制符/非输出控制符/源.c
+++
@@ -1,8 +0,0 @@
-# include<stdio.h>
-int main(viod)
-{
-	int i;
-		scanf("m%d", &i);
-		printf("i = %d\n", i);
-		return 0;
-}
